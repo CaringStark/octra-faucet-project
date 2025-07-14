d1e2910398446bb95c3b555c34ec4dde7a626d61
Caring Stark <166412829+CaringStark@users.noreply.github.com>
2025-07-14 20:45:54 +0100

    fix: create virtualenv and install Python deps inside it

--- build.sh
+++ build.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
-
 npm install
-pip3 install -r octra_pre_client/requirements.txt
+
+# Create Python virtual environment inside the project
+python3 -m venv octra_pre_client/venv
+
+# Activate it and install dependencies
+source octra_pre_client/venv/bin/activate
+pip install --upgrade pip
+pip install -r octra_pre_client/requirements.txt
