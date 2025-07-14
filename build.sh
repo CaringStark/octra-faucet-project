43301a40c91a1f252ca5a7c8fe3a72edd86f61e5
Caring Stark <166412829+CaringStark@users.noreply.github.com>
2025-07-14 21:54:53 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -3,10 +3,4 @@
 # Install Node dependencies
 npm install
 
-# Install pipx if not already available
-python3 -m ensurepip --upgrade
-python3 -m pip install --upgrade pip pipx
-python3 -m pipx ensurepath
-
-# Install Python requirements into isolated environment
-pipx install . --spec "aiohttp pynacl pyperclip"
+python3 -m pip install --break-system-packages -r octra_pre_client/requirements.txt
