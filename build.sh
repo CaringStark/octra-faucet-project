6e45bc719e1d1369342b3541cf6bf4d56a9d0d32
Caring Stark <166412829+CaringStark@users.noreply.github.com>
2025-07-15 01:39:21 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+
+# Create virtual environment
+python3 -m venv venv
+
+# Activate virtual environment
+source venv/bin/activate
+
+# Upgrade pip & install requirements
+pip install --upgrade pip
 pip install -r octra_pre_client/requirements.txt
