fb7af69ea40c67d00570a90e72549310811edd87
Caring Stark <166412829+CaringStark@users.noreply.github.com>
2025-07-15 02:03:13 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 
 const FAUCET_AMOUNT = 1; // fixed token amount per request
 const CLI_PATH = 'cli.py';
-const PYTHON = 'python3';
+const PYTHON = './.venv/bin/python3';
 
 app.use(bodyParser.json());
 const rateLimitMap = new Map(); // Store address into lastRequestTime
